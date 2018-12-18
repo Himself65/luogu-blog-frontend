e0b2ccd3075a1abeb908a0f91280ff4307e497c9
himself65 <himself6565@gmail.com>
2018-12-19 01:21:19 +0800

    BUMP

--- src/plugins/hljs.js
+++ src/plugins/hljs.js
@@ -1,6 +1,5 @@
-/* eslint-disable */
-import hljs from 'highlight.js/lib/highlight';
-import cpp from 'highlight.js/lib/languages/cpp';
+import hljs from 'highlight.js/lib/highlight'
+import cpp from 'highlight.js/lib/languages/cpp'
 
 window.hljs = hljs
 hljs.registerLanguage('cpp', cpp)
